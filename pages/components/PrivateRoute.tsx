f4cf2b44eef5841be4812a01e2d2d56205b987c0
andee <andrey14501450@gmail.com>
2022-06-27 10:36:28 +0300

    Added PrivateRoute component

---
+++ pages/components/PrivateRoute.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../logic/authSlice';
+import { Routes } from '../../utils/Routes';
+import Link from 'next/link';
+
+export function PrivateRoute({ children, href }) {
+    const user = useSelector(selectCurrentUser);
+
+    const isVerified = (): boolean => user.isVerified;
+
+    const userExists = (): boolean => user.email !== null && user.token !== null;
+
+    if (userExists() && !isVerified()) {
+        return <Link href={Routes.confirm}>{children}</Link>;
+    }
+
+    if (!userExists()) {
+        return <Link href={Routes.login}>{children}</Link>;
+    }
+
+    return <Link href={href}>{children}</Link>;
+}
